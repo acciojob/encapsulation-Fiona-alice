dc727c7fef537a071e34a40a4465ff29412c4257
Fiona-alice <alicce.fiona@gmail.com>
2022-12-14 08:23:16 +0300

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+    public static void main (String []args){
+        RWOnly obj = new RWOnly();
+        //name has private access in com.driver RWOnly
+        //obj.name ="Fiona"
+        obj.setName("Fiona");
+        System.out.println(obj.getName());
+    }
   
 }
